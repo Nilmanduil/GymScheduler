10d1ae190f877b13bcac95cd0fce86eb1cdc15a5
Thibault Goudouneix <thibault.goudouneix-prestataire@labanquepostale.fr>
2019-07-25 16:49:41 +0200

    Reducer for resume

--- src/components/TrainingWeekSummary.svelte
+++ src/components/TrainingWeekSummary.svelte
@@ -1,7 +1,28 @@
 <script>
+    export let summary;
+    export let days;
+    let isDataFormatted = false;
+    let formattedData;
 
+    function prepareSummary() {
+        formattedData = summary.reduce((acc, curr) => {
+            if (Object.keys(acc).includes(curr.title)) {
+                acc[curr.title] = [...acc[curr.title], curr];
+            } else {
+                acc[curr.title] = [curr];
+            }
+            return acc;
+        }, {});
+        isDataFormatted = true;
+        console.log(formattedData);
+    }
+
+    prepareSummary();
 </script>
 
 <style></style>
 
 <h2>Summary</h2>
+{#if isDataFormatted}
+    {formattedData}
+{/if}
